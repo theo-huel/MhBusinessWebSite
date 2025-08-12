45915f509b88ba2584a0d0bd4fa031af2cee5df5
Theoh <theo.huel30@gmail.com>
2025-08-12 16:15:54 +0200

    correction apparition citation

--- src/components/FadeInOnScrollCitation.jsx
+++ src/components/FadeInOnScrollCitation.jsx
@@ -4,12 +4,12 @@ import { useRef } from "react";
 
 const FadeInOnScrollLeft = ({ children, delay = 0 }) => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, margin: "100px" });
+  const isInView = useInView(ref, { once: true, margin: "0px" });
 
   return (
     <motion.div
       ref={ref}
-      initial={{ opacity: 0, x: 1000 }}
+      initial={{ opacity: 0, x: 300 }}
       animate={isInView ? { opacity: 1, x: 0 , y: 0 } : {}}
       transition={{ duration: 2, delay }}
     >
